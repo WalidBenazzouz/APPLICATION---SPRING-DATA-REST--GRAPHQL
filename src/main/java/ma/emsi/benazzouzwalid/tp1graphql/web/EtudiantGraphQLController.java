cc26c37bf6e31f2c31201b18dcb8360b516f5ae7
WalidBenazzouz <benazzouz.walid@outlook.com>
2025-11-28 12:08:40 +0100

    TP2: Refactor EtudiantGraphQLController to use DTO and Mapper

--- src/main/java/ma/emsi/benazzouzwalid/tp1graphql/web/EtudiantGraphQLController.java
+++ src/main/java/ma/emsi/benazzouzwalid/tp1graphql/web/EtudiantGraphQLController.java
@@ -10,24 +10,51 @@ import java.util.List;
 
 @Controller
 public class EtudiantGraphQLController {
-    @Autowired private EtudiantRepository etRepo;
-    @Autowired private CentreRepository ceRepo;
+    @Autowired
+    private EtudiantRepository etRepo;
+    @Autowired
+    private CentreRepository ceRepo;
+    @Autowired
+    private ma.emsi.benazzouzwalid.tp1graphql.mapper.DtoToEtudiant mapper;
 
     @QueryMapping
-    public List<Etudiant> listEtudiants(){ return etRepo.findAll(); }
+    public List<Etudiant> listEtudiants() {
+        return etRepo.findAll();
+    }
 
     @QueryMapping
-    public List<Centre> centres(){ return ceRepo.findAll(); }
+    public List<Centre> centres() {
+        return ceRepo.findAll();
+    }
+
+    @QueryMapping
+    public Etudiant getEtudiantById(@Argument Long id) {
+        return etRepo.findById(id).orElse(null);
+    }
 
     @MutationMapping
-    public Etudiant addEtudiant(@Argument EtudiantDTO dto){
-        Centre c = ceRepo.findById(dto.centreId()).orElse(null);
-        Etudiant e = Etudiant.builder()
-                .nom(dto.nom())
-                .prenom(dto.prenom())
-                .genre(dto.genre())
-                .centre(c)
-                .build();
+    public Etudiant addEtudiant(@Argument EtudiantDTO etudiant) {
+        Etudiant e = new Etudiant();
+        mapper.toEtudiant(e, etudiant);
         return etRepo.save(e);
+    }
+
+    @MutationMapping
+    public Etudiant updateEtudiant(@Argument Long id, @Argument EtudiantDTO etudiant) {
+        Etudiant e = etRepo.findById(id).orElse(null);
+        if (e != null) {
+            mapper.toEtudiant(e, etudiant);
+            return etRepo.save(e);
+        }
+        return null;
+    }
+
+    @MutationMapping
+    public Boolean deleteEtudiant(@Argument Long id) {
+        if (etRepo.existsById(id)) {
+            etRepo.deleteById(id);
+            return true;
+        }
+        return false;
     }
 }
